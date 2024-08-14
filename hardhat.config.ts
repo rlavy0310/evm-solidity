eb0d22e443156e3731f61ec86f102393e24582b4
kimpyo <kimpyo@kimpyodeMacBook-Pro.local>
2024-08-14 12:05:20 +0800

    test network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    bscTest: {
+      url: 'https://bsc-testnet.blockpi.network/v1/rpc/public	', // 输入您的RPC URL
+      chainId: 97, // (hex: 0x504),
+      accounts: [process.env.SECRET_KEY],
+    },
+  },
 };
 
 export default config;
