fdc81b1fa67fea38b97b7f89f49207817fd12fae
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-08-16 11:10:55 +0800

    Update 3.modifier-one-hack.sol

--- contracts/sol-tech/3.modifier-one-hack.sol
+++ contracts/sol-tech/3.modifier-one-hack.sol
@@ -1,3 +1,9 @@
+//gateOne ：tx.origin是发起交易的地址，msg.sender是调用的地址，这个通过部署合约再调用即可通过
+//gateTwo ：gasleft()指的是运行到当前指令还剩余的 gas 量，要能整除 8191。且external call 最低的gas限制是21000 ，所以暴力循环i + 8191 * 3 > 21000 ,成功执行再break
+//gateThree()：uint32(uint64(_gateKey)) == uint16(uint64(_gateKey) 需要判断后面的四个字节是 0000FFFF满足
+//             uint32(uint64(_gateKey)) != uint64(_gateKey)       判断uint64(_gateKey) 是满足八个字节的 0xFFFFFFFF0000FFFF
+//             uint32(uint64(_gateKey)) == uint16(uint160(tx.origin) 需要把tx.origin 转化成匹配的_gateKey，且满足上述需求
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract Attack {
