c5a35c86cb12f0137156b90b723461d67778444b
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-08-21 12:55:32 +0800

    Create IERC6551Registry.sol

---
+++ contracts/project/interfaces/IERC6551Registry.sol
@@ -0,0 +1,28 @@
+interface IERC6551Registry {
+
+    event AccountCreated(
+        address account,
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt
+    );
+
+    function createAccount(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 seed,
+        bytes calldata initData
+    ) external returns (address);
+
+    function account(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt
+    ) external view returns (address);
+}
