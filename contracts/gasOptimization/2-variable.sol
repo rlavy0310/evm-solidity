14f9c46f33f59ac46d250711b5594238301f1223
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-09-03 13:27:04 +0800

    Update and rename 2-calldata&memory.sol to 2-variable.sol

--- contracts/gasOptimization/2-variable.sol
+++ contracts/gasOptimization/2-variable.sol
@@ -21,3 +21,15 @@ contract CalldataAndMemory {
         xiaoming2 = xiaoming2_;
     }
 }
+
+contract Constant {
+    uint256 public constant varConstant = 1000;
+}
+
+contract Immutable {
+    uint256 public immutable varImmutable = 1000;
+}
+
+contract Public {
+    uint256 public variable = 1000;
+}
