0220473b050a7c6396cb84beb1c5e2aea071d31e
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-09-11 15:46:59 +0800

    Create ERC4337Factory.sol

---
+++ contracts/project/ERC4337/ERC4337Factory.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {LibClone} from "../utils/LibClone.sol";
+
+contract ERC4337Factory {
+
+    /// @dev Address of the ERC4337 implementation.
+    address public immutable implementation;
+
+    constructor(address erc4337) payable {
+        implementation = erc4337;
+    }
+
+    /// @dev Deploys an ERC4337 account with `salt` and returns its deterministic address.
+    /// If the account is already deployed, it will simply return its address.
+    /// Any `msg.value` will simply be forwarded to the account, regardless.
+    function createAccount(address owner, bytes32 salt) public payable virtual returns (address) {
+        // Check that the salt is tied to the owner if required, regardless.
+        LibClone.checkStartsWith(salt, owner);
+        // Constructor data is optional, and is omitted for easier Etherscan verification.
+        (bool alreadyDeployed, address account) =
+            LibClone.createDeterministicERC1967(msg.value, implementation, salt);
+
+        if (!alreadyDeployed) {
+            /// @solidity memory-safe-assembly
+            assembly {
+                mstore(0x14, owner) // Store the `owner` argument.
+                mstore(0x00, 0xc4d66de8000000000000000000000000) // `initialize(address)`.
+                if iszero(call(gas(), account, 0, 0x10, 0x24, codesize(), 0x00)) {
+                    returndatacopy(mload(0x40), 0x00, returndatasize())
+                    revert(mload(0x40), returndatasize())
+                }
+            }
+        }
+        return account;
+    }
+
+    /// @dev Returns the deterministic address of the account created via `createAccount`.
+    function getAddress(bytes32 salt) public view virtual returns (address) {
+        return LibClone.predictDeterministicAddressERC1967(implementation, salt, address(this));
+    }
+
+    /// @dev Returns the initialization code hash of the ERC4337 account (a minimal ERC1967 proxy).
+    /// Used for mining vanity addresses with create2crunch.
+    function initCodeHash() public view virtual returns (bytes32) {
+        return LibClone.initCodeHashERC1967(implementation);
+    }
+}
