31389eb00a4583eef31d5dec6ca404b7b7c904f7
rlavy0310 <57977123+rlavy0310@users.noreply.github.com>
2024-09-11 17:01:42 +0800

    Update testEntryPoint.sol

--- contracts/project/ERC4337/testEntryPoint.sol
+++ contracts/project/ERC4337/testEntryPoint.sol
@@ -31,3 +31,6 @@ contract testEntryPoint {
         depositTo(msg.sender);
     }
 }
+
+
+//https://biconomy.notion.site/ERC-4337-Decoding-EntryPoint-and-UserOperation-c9589d072041413486d2caef49260f9f
